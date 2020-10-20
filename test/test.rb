95544c861999d66d6c8615234502b861f6f07c00
Yoshiyuki Hirano <yhirano@me.com>
2020-10-20 13:41:22 +0900

    Bump oauth2 version

--- test/test.rb
+++ test/test.rb
@@ -24,7 +24,7 @@ class CallbackUrlTest < StrategyTestCase
     url_base = "http://auth.request.com"
     @request.stubs(:url).returns("#{url_base}/some/page")
     strategy.stubs(:script_name).returns("") # as not to depend on Rack env
-    assert_equal "#{url_base}/auth/slack/callback", strategy.callback_url
+    assert_equal "#{url_base}/auth/slack/callback", strategy.send(:callback_url)
   end
 
   test "returns path from callback_path option" do
@@ -32,7 +32,7 @@ class CallbackUrlTest < StrategyTestCase
     url_base = "http://auth.request.com"
     @request.stubs(:url).returns("#{url_base}/page/path")
     strategy.stubs(:script_name).returns("") # as not to depend on Rack env
-    assert_equal "#{url_base}/auth/slack/done", strategy.callback_url
+    assert_equal "#{url_base}/auth/slack/done", strategy.send(:callback_url)
   end
 end
 
